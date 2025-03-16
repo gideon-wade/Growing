173c86c13c5c08b9b6a9141a95afdfc31fc3d0d0
Viktor Kolind <s204166@student.dtu.dk>
2025-03-16 20:48:36 +0100

    added flag and fixed downscaling issue of tilemap

--- art/tileset/sketch/export.bat
+++ art/tileset/sketch/export.bat
@@ -1,1 +1,1 @@
-ffmpeg -i tileset.png -vf "scale=1024:1024:force_original_aspect_ratio=decrease,pad=1024:1024:(ow-iw)/2:(oh-ih)/2" tileset1024.png -y
+ffmpeg -i tileset.png -vf "scale=1024:1024:force_original_aspect_ratio=decrease:flags=neighbor,pad=1024:1024:(ow-iw)/2:(oh-ih)/2" tileset1024.png -y
